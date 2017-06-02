cb921273d06cb653206e4573e53f2d7cf16b74fb
Leo <leo.cc0123@gmail.com>
2017-06-02 16:53:44 -0700

    add settings and start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import Expression from "./Expression.jsx";
 import NumPad from "./NumPad.jsx";
 import ExpressionHelper from "./ExpressionHelper.js";
 import $ from "jquery";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Dialog from "material-ui/Dialog";
+import TextField from "material-ui/TextField";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,25 +30,39 @@ export default class App extends React.Component {
       currentExpression: ExpressionHelper.getCurrentExpression(),
       answer: "",
       message: "",
-      remaining: ExpressionHelper.getRemaining()
+      remaining: ExpressionHelper.getRemaining(),
+      dialogOpen: true,
+      config: this.config
     };
     this.numButtonClick = this.numButtonClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleConfigMaxChange = this.handleConfigMaxChange.bind(this);
+    this.handleConfigCountChange = this.handleConfigCountChange.bind(this);
+    this.getDialogStatus = this.getDialogStatus.bind(this);
     window.numPress = this.numButtonClick;
+    window.getDialogStatus = this.getDialogStatus;
     window.onkeyup = function(e) {
+      if (window.getDialogStatus()) {
+        return;
+      }
       var key = e.keyCode
         ? e.keyCode
         : e.which;
 
-      if (48<=key && key<=57) {
-        window.numPress(key-48);
-      } else if (96<=key && key<=105) {
-        window.numPress(key-96);
+      if (48 <= key && key <= 57) {
+        window.numPress(key - 48);
+      } else if (96 <= key && key <= 105) {
+        window.numPress(key - 96);
       } else if (key == 13) {
         window.numPress("OK");
       } else if (key == 110) {
         window.numPress("C");
       }
     };
+  }
+
+  getDialogStatus() {
+    return this.state.dialogOpen;
   }
 
   load() {
@@ -96,6 +113,22 @@ export default class App extends React.Component {
     this.setState({
       answer: this.state.answer + e
     });
+  }
+
+  handleClose() {
+    this.config.date = new Date();
+    console.log(this.config);
+    this.setState({dialogOpen: false});
+  }
+
+  handleConfigMaxChange(e, v) {
+    this.config.max = v;
+    this.setState({config: this.config});
+  }
+
+  handleConfigCountChange(e, v) {
+    this.config.count = v;
+    this.setState({config: this.config});
   }
 
   /*check() {
@@ -154,18 +187,33 @@ export default class App extends React.Component {
   }*/
 
   render() {
+    const actions = [
+      <button onClick = {this.handleClose} className = "btn btn-default" style = {this.style} > start </button>
+    ];
+
     return (
       <div >
-        <div className="row" style={this.centerStyle}>
+        <MuiThemeProvider>
+          <Dialog title="settings" actions={actions} modal={true} open={this.state.dialogOpen}>
+            <div className="row" style={this.centerStyle}>
+              max:<TextField hintText="number" style={this.style}
+                defaultValue={this.state.config.max} onChange={this.handleConfigMaxChange}/>
+            </div>
+            <div className="row" style = {this.centerStyle}>
+              count : <TextField hintText="number" style={this.style}
+                defaultValue={this.state.config.count} onChange={this.handleConfigCountChange}/>
+            </div>
+          </Dialog>
+        </MuiThemeProvider>
+        <div className = "row" style = {this.centerStyle}>
           <div style={this.style}>{this.state.remaining}</div>
         </div>
         <div className="row" style={this.centerStyle}>
           <Expression data={this.state.currentExpression} answer={this.state.answer} message={this.state.message}/>
         </div>
-        <div className="row" style={this.centerStyle}>
+        <div className = "row" style = {this.centerStyle} >
           <NumPad numButtonClick={this.numButtonClick}/>
         </div>
-      </div>
-    );
+      </div>);
+    }
   }
-}
