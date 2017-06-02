116a4026f567a40be703fe2a711bbf19e5d63799
Leo <leo.cc0123@gmail.com>
2017-06-02 15:01:24 -0700

    yarn test ok on delta laptop"

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './components/App.jsx';
-import "keypress";
 
 //import injectTapEventPlugin from 'react-tap-event-plugin';
 //import FlatButton from "material-ui/FlatButton";
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 //injectTapEventPlugin();
-<<<<<<< HEAD
-var listener = new window.keypress.Listener();
-console.log(listener);
-=======
->>>>>>> 9ada37950c7db356f5f07e1854730eb574609337
+
 ReactDOM.render( < App / > , document.getElementById('root'));
 
 
