389bba9a1ce5551782c11444af73651f911356fb
Leo <leo.cc0123@gmail.com>
2017-06-02 14:14:15 -0700

    before merge

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './components/App.jsx';
+import "keypress";
 
 //import injectTapEventPlugin from 'react-tap-event-plugin';
 //import FlatButton from "material-ui/FlatButton";
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 //injectTapEventPlugin();
-
+var listener = new window.keypress.Listener();
+console.log(listener);
 ReactDOM.render( < App / > , document.getElementById('root'));
 
 
